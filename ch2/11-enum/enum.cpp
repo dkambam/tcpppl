59e583d77eb8596629b62d3ad837e1d20719882c
Dheeraj K Reddy <kdr.code@gmail.com>
2015-06-01 13:41:53 -0400

    Add enum/

---
+++ ch2/11-enum/enum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+enum class Color { Red, Green, Blue };
+enum class TrafficLight { Green, Yellow, Red }; // notice no name conflict
+
+int main(){
+	std::cout << "nothing to print. Check source code.\n";
+}
